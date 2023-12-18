144e06785500bec69fe4a40c4ae127c1e79b3a21
Nicholas Weist <104579319+AspiringIT@users.noreply.github.com>
2023-12-18 10:47:25 -0600

    Update Eject.js
    Changed how discs eject instead of using relaying on broken 3rd party software. I am using powershelgl on windows and for Mac Darwin

--- Eject.js
+++ Eject.js
@@ -9,30 +9,38 @@ colors.setTheme({
     dash: 'gray'
 });
 
-function ejectAllDVDs() {
-  const ejectCommand = process.platform === 'win32' ?
-    'powershell -Command "(New-Object -comObject "Shell.Application").Namespace(17).ParseName(\"D:\").InvokeVerb(\"Eject\")"' :
-    process.platform === 'darwin' ? 'drutil eject' :
-    'eject';
+function ejectDVDs() {
+  if (eject === "true") {
+    const ejectCommand =
+      process.platform === 'win32'
+        ? 'powershell -Command "(New-Object -comObject \\"Shell.Application\\").Namespace(17).ParseName(\\"D:\\").InvokeVerb(\\"Eject\\")"'
+        : process.platform === 'darwin'
+        ? 'drutil eject'
+        : 'eject';
 
-  exec(ejectCommand, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.error(`stderr: ${stderr}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
+    exec(ejectCommand, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.error(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
 
-    // Print a message after discs have been ejected
-    console.log('All DVDs have been ejected.');
+      // Print a message after discs have been ejected
+      console.log('All DVDs have been ejected.');
 
-    // Exit the program after ejecting the discs
+      // Exit the program after ejecting the discs
+      process.exit();
+    });
+  } else {
+    console.log(
+      colors.time(moment().format("LTS")) +
+        colors.dash(" - ") +
+        colors.info("Eject DVDs is disabled.")
+    );
     process.exit();
-  });
+  }
 }
-
-// Call the function to eject all DVDs
-ejectAllDVDs();
